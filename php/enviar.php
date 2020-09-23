5957961d159e2dac1e8f40391a6ca26aea99439e
957643934 <70980532+957643934@users.noreply.github.com>
2020-09-23 08:56:16 -0500

    Add files via upload

--- php/enviar.php
+++ php/enviar.php
@@ -1,10 +1,17 @@
 <?php  
-	$destino= "cjun.jhojan.aylas.p20@gmail.com";
-	$nombre = $_POST["nombre"];
-	$correo = $_POST["correo"];
-	$telefono = $_POST["telefono"];
-	$mensaje = $_POST["mensaje"];
-	$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono: " . $telefono . "\nMensaje: " . $mensaje; 
-	mail($destino,"Contacto", $contenido);
-	header("Location:gracias.html");
-?>
+	
+	if (isset($_POST['enviar'])) {
+		if (!empty($_POST['name']) && !empty($_POST['asunto']) && !empty($_POST['msg']) && !empty($_POST['email'])) {
+			$name = $_POST['name'];
+			$asunto = $_POST['asunto'];
+			$msg = $_POST['msg'];
+			$email = $_POST['email'];
+			$header = "From: noreply@example.com" . "\r\n"
+			$header = "Reply-To: noreply@example.com" . "\r\n"
+			$header = "X-Mailer: PHP/" . phpversion();
+			$mail = @mail($email,$asunto,$msg,$header);
+			if ($mail) {
+				echo "<h4>¡Mail enviado exitosamente!</h4>";
+			}
+	}
+}

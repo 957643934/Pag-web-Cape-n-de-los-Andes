8e9472df4e83c5dfbdeebebe461083592295f251
957643934 <70980532+957643934@users.noreply.github.com>
2020-09-23 09:21:47 -0500

    Add files via upload

--- php/enviar.php
+++ php/enviar.php
@@ -2,6 +2,7 @@
 	
 	if (isset($_POST['enviar'])) {
 		if (!empty($_POST['name']) && !empty($_POST['asunto']) && !empty($_POST['msg']) && !empty($_POST['email'])) {
+			$destinatario = 'cjun.jhojan.aylas.p20@gmail.com';
 			$name = $_POST['name'];
 			$asunto = $_POST['asunto'];
 			$msg = $_POST['msg'];
@@ -9,9 +10,9 @@
 			$header = "From: noreply@example.com" . "\r\n"
 			$header = "Reply-To: noreply@example.com" . "\r\n"
 			$header = "X-Mailer: PHP/" . phpversion();
-			$mail = @mail($email,$asunto,$msg,$header);
+			$mail = @mail($destinatario,$email,$asunto,$msg,$header);
 			if ($mail) {
 				echo "<h4>¡Mail enviado exitosamente!</h4>";
 			}
 	}
-}
+?>
